ed151a6588b4c1007af945b29ec78432e7d0d90f
unknown <fboy4069@gmail.com>
2022-05-27 12:35:08 +0600

    my profile added

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -26,17 +26,32 @@ const Review = () => {
 
     };
     return (
-        <div className=' items-center justify-center'>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input className='mb-2 appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='User Image URL' type="text" {...register("userImage")} />
-                <input className='mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Your name' type='text' {...register("userName")} />
-                <input className='mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='product name' type='text' {...register("productName")} />
-                <input className='mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='give your rating' type='text' {...register("rating")} />
-                <textarea className='mb-2 appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write your comment' type="text" {...register("comment")} />
+        <section>
+            <h1 className='text-4xl mb-4 mt-5  text-center font-bold text-violet-800'>Give us your feedback below</h1>
+            <div className=' items-center justify-center'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='mb-2 appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Your Image URL' type="text" {...register("userImage")} />
+                    <input className='mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Your name' type='text' {...register("userName")} />
+                    <input className='mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='product name' type='text' {...register("productName")} />
+                    <select type="text" className="select mb-2 appearance-none rounded-none relative block w-full px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm max-w-xs"{...register("rating")}>
+                        <option disabled selected>Select Your Review</option>
 
-                <input className=' bg-primary hover:bg-violet-600 text-white block w-full px-3 py-3' value='Add a review' type="submit" />
-            </form>
-        </div>
+                        <option>5</option>
+                        <option>4.5</option>
+                        <option>4</option>
+                        <option>3.5</option>
+                        <option>3</option>
+                        <option>2.5</option>
+                        <option>2</option>
+                        <option>1.5</option>
+                        <option>1</option>
+                    </select>
+                    <textarea className='mb-2 appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write your comment' type="text" {...register("comment")} />
+
+                    <input className=' bg-primary hover:bg-violet-600 text-white block w-full px-3 py-3' value='Add a review' type="submit" />
+                </form>
+            </div>
+        </section>
     );
 };
 
